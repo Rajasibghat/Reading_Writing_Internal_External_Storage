c0fe37cb15e3ac2c0abefda0b3faff7510ffe70f
Sibghat65 <rajasibghat2@gmail.com>
2022-11-16 21:19:34 -0800

    Reading and Writing TextFiles to Internal Storage

--- app/src/main/java/com/example/readwritetextfile/MainActivity.java
+++ app/src/main/java/com/example/readwritetextfile/MainActivity.java
@@ -12,10 +12,14 @@ import android.widget.Toast;
 
 import com.example.readwritetextfile.databinding.ActivityMainBinding;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.lang.ref.WeakReference;
 
@@ -62,6 +66,84 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        binding.btnReadMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    InputStream inputStream=openFileInput("testFile.txt");
+                    int character;
+                    StringBuilder stringBuilder=new StringBuilder();
+                    while((character=inputStream.read())!=-1){
+                        stringBuilder.append((char)character);
+                    }
+                    inputStream.close();
+                    binding.textMainDirectory.setText(stringBuilder.toString());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        binding.btnReadSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                File path=new File(getFilesDir(),"TextFilesFolder");
+                File fileName=new File(path,"test2file.txt");
+                try {
+                    FileInputStream inputStream=new FileInputStream(fileName);
+                    int character;
+                    StringBuilder builder=new StringBuilder();
+                    while ((character=inputStream.read())!=-1){
+                        builder.append((char)character);
+                    }
+                    inputStream.close();
+                    binding.textSubDirectory.setText(builder.toString());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+
     }
+
+    //This is another method to read line by line main directory
+    private void readMainDirectoryLineByLine(){
+        try {
+            BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(openFileInput("testFile.txt")));
+            String line;
+            StringBuilder builder=new StringBuilder();
+            while ((line=bufferedReader.readLine())!=null){
+                builder.append(line);
+            }
+            bufferedReader.close();
+            binding.textMainDirectory.setText(builder.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //This is another method to read line by line sub directory
+    private void readSubDirectoryLineByLine(){
+        File path=new File(getFilesDir(),"TextFilesFolder");
+        File fileName=new File(path,"test2file.txt");
+        try {
+            FileInputStream inputStream=new FileInputStream(fileName);
+            InputStreamReader inputStreamReader=new InputStreamReader(inputStream);
+            BufferedReader reader=new BufferedReader(inputStreamReader);
+            String line;
+            StringBuilder builder=new StringBuilder();
+            while ((line=reader.readLine())!=null){
+                builder.append(line);
+            }
+            reader.close();
+            binding.textSubDirectory.setText(builder.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 
 }
